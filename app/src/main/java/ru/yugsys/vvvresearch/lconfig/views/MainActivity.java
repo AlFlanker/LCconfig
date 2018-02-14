10ed28bd8f2a81573e2abaef47135704e597352c
vvvresearch <vvvresearch@gmail.com>
2018-02-14 11:06:21 +0300

    Add gradle files

--- app/src/main/java/ru/yugsys/vvvresearch/lconfig/views/MainActivity.java
+++ app/src/main/java/ru/yugsys/vvvresearch/lconfig/views/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.yugsys.vvvresearch.lconfig.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Intent loginIntent = new Intent(this, LoginActivity.class );
+        startActivity(loginIntent);
+
     }
 
     @Override
